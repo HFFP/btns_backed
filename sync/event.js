48a2918fafcd6ebdb7344733cf2b6b5b4c37f1ba
hffp <510033939@qq.com>
2019-12-13 13:49:58 +0800

    decode event log

--- sync/event.js
+++ sync/event.js
@@ -1,9 +1,25 @@
 const BTNS_ABI = require('./btnsAbi')
 const config = require('config')
-const axios = require('axios')
-const CronJob = require('cron').CronJob
+const axios = require('axios').default
 const util = require('../module/utils')
+const Web3 = require('web3')
 
+const web3 = new Web3();
+
+const events = function(){
+  const event = {}
+  BTNS_ABI.map(item => {
+    if (item.type === 'event') {
+      const paramsTypeStr = item.inputs.map(param => {
+        return param.type
+      }).join(',')
+      const sha3 = web3.utils.sha3(`${item.name}(${paramsTypeStr})`)
+      const input = item.inputs
+      event[sha3] = {sha3, input, name: item.name}
+    }
+  })
+  return event;
+}();
 
 
 async function syncContractLogs () {
@@ -11,18 +27,29 @@ async function syncContractLogs () {
   const start = config.startBlock;
 
   const api = `${config.etherScan}/?module=logs&action=getlogs&fromBlock=${start}&toBlock=latest&address=${config.btnsContract}&apikey=${config.apiKey}&sort=-timestamp`
-  console.log(api)
   const {data} = await axios.get(api);
-  console.log(data);
+  data.result.forEach(item => {
+    const topic = item.topics[0]
+    const logs = web3.eth.abi.decodeLog(
+      events[topic].input,
+      item.data,
+      item.topics.slice(1, item.topics.length)
+    )
+    console.log(logs, events[topic].name)
+  })
 }
 
 
 async function startSyncContractLogs () {
   while (true) {
-    const startTime = Date.now()
-    await syncContractLogs()
-    const duration = Date.now() - startTime
-    await util.sleep(duration > 10000 ? 0 : 1000 - duration)
+    try {
+      const startTime = Date.now()
+      await syncContractLogs()
+      const duration = Date.now() - startTime
+      await util.sleep(duration > 10000 ? 0 : 10000 - duration)
+    } catch (e) {
+      console.error(e)
+    }
   }
 }
 
