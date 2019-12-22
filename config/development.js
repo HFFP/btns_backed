1a0ae224af785afdf37d5b7fc51ce9b13f96fc63
hffp <510033939@qq.com>
2019-12-23 02:53:51 +0800

    pm2 config

--- config/development.js
+++ config/development.js
@@ -6,7 +6,7 @@ module.exports = {
   etherScan: 'http://api-ropsten.etherscan.io/api',
   btnsContract: '0x19467914271365e427caC6d5C478d6523d8FfC3a',
   startBlock: 6956250,
-  apiKey: '',
+  apiKey: 'KRV9D6XFK98S4MTK8TVQPFVBV9BVGRWPE6',
   ethAddress: '0x3460bc9e7059ad8b1245d1961e90b121f8b2bcba',
   ethPrivateKey: 'F8FE18C01C58B4C4740A7EDC65152B5964979D017B5D787F1D55EF1F67E0698D',
   network: 'ropsten',
