9db2cc9a48f7f36d14d24519e1a419cf113ffceb
hffp <510033939@qq.com>
2019-12-23 02:25:28 +0800

    pm2 config

--- config/development.js
+++ config/development.js
@@ -2,5 +2,13 @@ module.exports = {
   mongodb: {
     name: 'local',
     connectionStringURI: 'mongodb://127.0.0.1:27017/btns'
-  }
+  },
+  etherScan: 'http://api-ropsten.etherscan.io/api',
+  btnsContract: '0x19467914271365e427caC6d5C478d6523d8FfC3a',
+  startBlock: 6956250,
+  apiKey: '',
+  ethAddress: '0x3460bc9e7059ad8b1245d1961e90b121f8b2bcba',
+  ethPrivateKey: 'F8FE18C01C58B4C4740A7EDC65152B5964979D017B5D787F1D55EF1F67E0698D',
+  network: 'ropsten',
+  provider: "https://ropsten.infura.io/v3/3248083ccd694c02809beab6472443f2",
 }
